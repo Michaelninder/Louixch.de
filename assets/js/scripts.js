04916f17a42fabc92c92d46ea7c5baedbd48634e
Michaelninder <fabianeternis@gmail.com>
2025-12-28 19:48:31 +0100

    feat: Rename "Gepaecktraeger-Zug" to "Zug-Gepaecktraeger" for better UI

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -208,7 +208,7 @@ async function initSkinGallery() {
         "2025-Schaf.png",
         "2025-Soviet.png",
         "2025-Vintage.png",
-        "2025-Gepaecktraeger-Zug.png",
+        "2025-Zug-Gepaecktraeger.png",
 
         //"2024-Skin9.png",
         //"2024-Skin11.png",
